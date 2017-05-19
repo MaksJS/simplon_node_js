72b0fc0fdf0331a540e20ec50ce5696af21eb405
Maks <maxence.derous@gmail.com>
2017-05-19 16:26:21 +0200

    code style

--- fs_simple_example.js
+++ fs_simple_example.js
@@ -5,6 +5,6 @@ console.log(`Current directory is ${__dirname}`)
 const PATH = 'misc/foo.txt'
 
 fs.readFile(`${__dirname}/${PATH}`, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log("Content:", data);
+    if (err) throw err
+    console.log("Content:", data)
 });
