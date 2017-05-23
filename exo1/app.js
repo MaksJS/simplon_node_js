1b57cef0e6c9205a1cc48c302b2e5dde0f3b27dd
Maks <maxence.derous@gmail.com>
2017-05-23 16:44:05 +0200

    push structure of first exercise

---
+++ exo1/app.js
@@ -0,0 +1,8 @@
+// importer le module "myFs"
+// importer la config
+
+// lire le fichier /"chemin_dans_la_config"/foo.txt et afficher le contenu dans la console
+
+// écrire dans le fichier /"chemin_dans_la_config"/bar.txt
+
+// lire le fichier /"chemin_dans_la_config"/bar.txt et afficher le contenu dans la console
---
+++ exo1/myFs.js
@@ -0,0 +1,7 @@
+// importer le "vrai" module fs pour l'utiliser
+
+// fonction readFileUtf8(filename, callback)
+
+// fonction ....FileUtf8(filename, content, callback)
+
+// exporter les deux fonctions
